fc6a97ee5fba07b508f74e4776f4f5f861ff4563
Glen Keane <glenkeane.94@gmail.com>
2017-04-09 13:40:58 +0100

    working as expected

--- index.js
+++ index.js
@@ -1,28 +1,33 @@
 const nodegit = require('nodegit')
-// const minimatch = require('minimatch')
 const stackTrace = require('stack-trace')
-const path = require('path')
 const walkup = require('walk-up')
+const minimatch = require('minimatch')
+const path = require('path')
 
 function didPathChangeSinceCommit (oid, searchPath, cb) {
   const callerFile = stackTrace.get()[0].getFileName()
   const absolutePath = path.resolve(callerFile, searchPath)
 
-  walkup(callerFile, '.git', (err, result) => {
+  walkup(callerFile, '.git', (err, repoResult) => {
     if (err) return cb(err)
-    if (!result.found) return cb(new Error('cant find a git repo'))
+    if (!repoResult.found) return cb(new Error('cant find a git repo'))
 
-    nodegit.Repository.open(result.path)
+    nodegit.Repository.open(repoResult.path)
       .then(repo => Promise.all([repo.getCommit(oid), repo.getHeadCommit()]))
       .then(commits => Promise.all(commits.map(commit => commit.getTree())))
       .then(trees => trees[0].diff(trees[1]))
       .then(diff => diff.patches())
       .then(patches => patches.map(patch => {
-        console.log('diff', patch.oldFile().path(), patch.newFile().path())
+        const oldPath = path.join(repoResult.path, patch.oldFile().path())
+        const newPath = path.join(repoResult.path, patch.newFile().path())
+        return {oldPath, newPath}
       }))
-      .then(() => {
+      .then((paths) => {
+        paths.forEach(({oldPath, newPath}) => console.log('diff', oldPath, newPath))
+        const matched = paths.map(path => path.newPath).filter(minimatch.filter(absolutePath, {matchBase: true}))
+        console.error('matched', matched)
         console.error('path', absolutePath)
-        return cb(null, true)
+        return cb(null, Boolean(matched.length))
       })
       .catch(cb)
   })
