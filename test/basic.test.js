0b2aa111f78eaaaa6cdf64b9a0a0d48aea31212c
Glen Keane <glenkeane.94@gmail.com>
2017-04-09 13:41:07 +0100

    working as expected

--- test/basic.test.js
+++ test/basic.test.js
@@ -2,7 +2,7 @@ const test = require('tap').test
 const gitQuery = require('../')
 
 test('The readme changed since the commit that updated the readme', (t) => {
-  gitQuery.didPathChangeSinceCommit('267775ba3b6813303fad3eb307d436d812783568', '../*.md', (err, changed) => {
+  gitQuery.didPathChangeSinceCommit('fc6a97e', '../*.md', (err, changed) => {
     t.error(err)
     t.equal(true, changed)
     t.end()
