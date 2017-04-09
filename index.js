b500d441e0d7d652a8243d822289142d8a631a59
Glen Keane <glenkeane.94@gmail.com>
2017-04-09 13:27:15 +0100

    adding a diffed file

--- index.js
+++ index.js
@@ -1,5 +1,31 @@
-function didPathChangeSinceCommit (path) {
-  return false
+const nodegit = require('nodegit')
+// const minimatch = require('minimatch')
+const stackTrace = require('stack-trace')
+const path = require('path')
+const walkup = require('walk-up')
+
+function didPathChangeSinceCommit (oid, searchPath, cb) {
+  const callerFile = stackTrace.get()[0].getFileName()
+  const absolutePath = path.resolve(callerFile, searchPath)
+
+  walkup(callerFile, '.git', (err, result) => {
+    if (err) return cb(err)
+    if (!result.found) return cb(new Error('cant find a git repo'))
+
+    nodegit.Repository.open(result.path)
+      .then(repo => Promise.all([repo.getCommit(oid), repo.getHeadCommit()]))
+      .then(commits => Promise.all(commits.map(commit => commit.getTree())))
+      .then(trees => trees[0].diff(trees[1]))
+      .then(diff => diff.patches())
+      .then(patches => patches.map(patch => {
+        console.log('diff', patch.oldFile().path(), patch.newFile().path())
+      }))
+      .then(() => {
+        console.error('path', absolutePath)
+        return cb(null, true)
+      })
+      .catch(cb)
+  })
 }
 
 module.exports = {
--- test/basic.test.js
+++ test/basic.test.js
@@ -1,6 +1,10 @@
 const test = require('tap').test
+const gitQuery = require('../')
 
-test('this should pass', (t) => {
-  t.equal(1, 1)
-  t.end()
+test('The readme changed since the commit that updated the readme', (t) => {
+  gitQuery.didPathChangeSinceCommit('267775ba3b6813303fad3eb307d436d812783568', '../*.md', (err, changed) => {
+    t.error(err)
+    t.equal(true, changed)
+    t.end()
+  })
 })
